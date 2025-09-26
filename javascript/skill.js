60a5111d4ce694c783e80425b0becd0d5f3400b1
GautamPrasid <g.prasud14@gmail.com>
2025-09-26 13:52:26 +0545

    a lot of changes hard to track

---
+++ javascript/skill.js
@@ -0,0 +1,4 @@
+// Placeholder for any interactivity (if needed in the future)
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Page Loaded");
+});
--- skill.js
+++
@@ -1,4 +0,0 @@
-// Placeholder for any interactivity (if needed in the future)
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Page Loaded");
-});
